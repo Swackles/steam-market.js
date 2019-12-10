a0cc2fd48cf0b37fe228eef6a71cfd177767c214
Swackles <svenerik.rebane@gmail.com>
2019-12-10 19:19:44 +0200

    improvements to index

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const Search = require('./lib/search');
 const urlBuilder = require('./lib/urlBuilder');
 const request = require('./lib/request');
 
-class SteamMarket {
-  constructor(options = null) {
-  }
-
+module.exports = {
   async getPopular(start = 0, currency = "USD", count = 10, appId = null) {
     const url = urlBuilder.generateSearch(start, currency, count, appId, "");
     let data = await request(url);
@@ -14,6 +11,3 @@ class SteamMarket {
     else return null;
   }
 }
-module.exports = SteamMarket;
-
-let sm = new SteamMarket();
