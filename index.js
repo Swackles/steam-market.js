c7b363f2552c97efd83f2461e343357fff4cb58b
Swackles <svenerik.rebane@gmail.com>
2019-12-30 23:32:14 +0200

    added urlEncoding & findquery

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const urlBuilder = require('./lib/urlBuilder');
 const request = require('./lib/request');
 
 module.exports = {
+  async findItem(query, currency = "USD", appId = null) {
+    const url = urlBuilder.generateSearch(query, currency, appId);
+    let data = await request(url);
+
+    if (data) return new Search(data);
+    else return null;
+  },
+
   async getPopular(start = 0, currency = "USD", count = 10, appId = null) {
-    const url = urlBuilder.generateSearch(start, currency, count, appId, "");
+    const url = urlBuilder.generatePopular(start, currency, count, appId, "");
     let data = await request(url);
 
     if (data) return new Search(data);
